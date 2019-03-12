aa8a12357dbdab62db978147ce6f5165b1740113
sky_lock <sho59sky@outlook.jp>
2019-03-12 18:19:57 +0900

    start.bashを追加

---
+++ start.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cp -f -v "L:\Workspace\Bukkit\MoCar\target\mocar-1.0-SNAPSHOT.jar" "L:\Minecraft\01_Server\18_dev1.12.2\plugins"
+cd "\Minecraft\01_Server\18_dev1.12.2"
+sh start.sh
