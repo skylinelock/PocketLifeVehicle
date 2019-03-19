dda15d7767a86c74272c4828166cb6bf87d352af
sky_lock <sho59sky@outlook.jp>
2019-03-20 03:03:13 +0900

    車アイテムを右クリック以外で設置できなくする

--- src/main/java/dev/sky_lock/mocar/listener/PlayerListener.java
+++ src/main/java/dev/sky_lock/mocar/listener/PlayerListener.java
@@ -1,11 +1,17 @@
 package dev.sky_lock.mocar.listener;
 
-import dev.sky_lock.mocar.car.*;
+import dev.sky_lock.mocar.car.CarEntities;
+import dev.sky_lock.mocar.car.CarModel;
+import dev.sky_lock.mocar.car.CraftCar;
+import dev.sky_lock.mocar.car.ModelList;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
@@ -19,6 +25,12 @@ public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         ItemStack itemStack = event.getItem();
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        if (event.getBlockFace() != BlockFace.UP){
+            return;
+        }
         CarModel model = ModelList.getModel(itemStack);
         if (model == null) {
             return;
@@ -27,7 +39,9 @@ public class PlayerListener implements Listener {
         event.setUseInteractedBlock(Event.Result.DENY);
         event.setUseItemInHand(Event.Result.DENY);
         event.getPlayer().getInventory().remove(itemStack);
-        CarEntities.spawn(event.getPlayer().getUniqueId(), model, event.getPlayer().getLocation());
+        Location whereToSpawn = event.getClickedBlock().getLocation();
+        whereToSpawn.add(0, 1.0, 0);
+        CarEntities.spawn(event.getPlayer().getUniqueId(), model, whereToSpawn);
     }
 
     @EventHandler
